14751428bce67d4a57d25bf2a9aaf6320b7ce845
Uzziah Antredog <uantredogbusiness@gmail.com>
2023-12-05 11:06:38 +0200

    added: marry chrismas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload i love web111.
+          Edit <code>src/App.js</code> and save to reload Marry Christmas.
         </p>
         <a
           className="App-link"
